cfdf06dc7b3aa9345325af719d2bec27554d9de5
Stefan Plantikow <stefan.plantikow@googlemail.com>
2012-04-16 01:09:11 +0200

    Reduce libmath size a bit by importing smarter

--- src/libmath/math_impl/f64.rs
+++ src/libmath/math_impl/f64.rs
@@ -1,54 +1,66 @@
 type t = f64;
 
-const NaN: t = 0.0_f64/0.0_f64;
-const infinity: t = 1.0_f64/0.0_f64;
-const neg_infinity: t = -1.0_f64/0.0_f64;
+const NaN: f64 = 0.0_f64/0.0_f64;
+const infinity: f64 = 1.0_f64/0.0_f64;
+const neg_infinity: f64 = -1.0_f64/0.0_f64;
 
 mod consts {
+
+    import cmath::consts::*;
+
+    // Type abstract constants for writing generic floating point code
+
+    const zero: f64 = 0_f64;
+    const one: f64 = 1_f64;
+    const two: f64 = 2_f64;
+    const three: f64 = 3_f64;
+    const four: f64 = 4_f64;
+    const ten: f64 = 10_f64;
+
     // FIXME this is wrong! replace with hexadecimal (%a) constants below
     // (see Issue #1433)
-    const min_value: t = 2.225074e-308_f64;
-    const max_value: t = 1.797693e+308_f64;
-    const epsilon: t = 2.220446e-16_f64;
+    const min_value: f64 = 2.225074e-308_f64;
+    const max_value: f64 = 1.797693e+308_f64;
+    const epsilon: f64 = 2.220446e-16_f64;
 
     #[doc = "Archimedes' constant"]
-    const pi: t = 3.14159265358979323846264338327950288_f64;
+    const pi: f64 = 3.14159265358979323846264338327950288_f64;
 
     #[doc = "pi/2.0"]
-    const frac_pi_2: t = 1.57079632679489661923132169163975144_f64;
+    const frac_pi_2: f64 = 1.57079632679489661923132169163975144_f64;
 
     #[doc = "pi/4.0"]
-    const frac_pi_4: t = 0.785398163397448309615660845819875721_f64;
+    const frac_pi_4: f64 = 0.785398163397448309615660845819875721_f64;
 
     #[doc = "1.0/pi"]
-    const frac_1_pi: t = 0.318309886183790671537767526745028724_f64;
+    const frac_1_pi: f64 = 0.318309886183790671537767526745028724_f64;
 
     #[doc = "2.0/pi"]
-    const frac_2_pi: t = 0.636619772367581343075535053490057448_f64;
+    const frac_2_pi: f64 = 0.636619772367581343075535053490057448_f64;
 
     #[doc = "2.0/sqrt(pi)"]
-    const frac_2_sqrtpi: t = 1.12837916709551257389615890312154517_f64;
+    const frac_2_sqrtpi: f64 = 1.12837916709551257389615890312154517_f64;
 
     #[doc = "sqrt(2.0)"]
-    const sqrt2: t = 1.41421356237309504880168872420969808_f64;
+    const sqrt2: f64 = 1.41421356237309504880168872420969808_f64;
 
     #[doc = "1.0/sqrt(2.0)"]
-    const frac_1_sqrt2: t = 0.707106781186547524400844362104849039_f64;
+    const frac_1_sqrt2: f64 = 0.707106781186547524400844362104849039_f64;
 
     #[doc = "Euler's number"]
-    const e: t = 2.71828182845904523536028747135266250_f64;
+    const e: f64 = 2.71828182845904523536028747135266250_f64;
 
     #[doc = "log2(e)"]
-    const log2_e: t = 1.44269504088896340735992468100189214_f64;
+    const log2_e: f64 = 1.44269504088896340735992468100189214_f64;
 
     #[doc = "log10(e)"]
-    const log10_e: t = 0.434294481903251827651128918916605082_f64;
+    const log10_e: f64 = 0.434294481903251827651128918916605082_f64;
 
     #[doc = "ln(2.0)"]
-    const ln_2: t = 0.693147180559945309417232121458176568_f64;
+    const ln_2: f64 = 0.693147180559945309417232121458176568_f64;
 
     #[doc = "ln(10.0)"]
-    const ln_10: t = 2.30258509299404568401799145468436421_f64;
+    const ln_10: f64 = 2.30258509299404568401799145468436421_f64;
 /*
     const min_value: c_double = 0x1p-1022_f64;
     const max_value: c_double = 0x1.fffffffffffffp+1023_f64;
--- src/libmath/math_impl/float.rs
+++ src/libmath/math_impl/float.rs
@@ -1,28 +1,34 @@
 type t = float;
+type m = fXY::t;
 
-// Keep this in sync with impl_f32 and impl_f64
-
-const NaN: t = fXY::NaN as t;
-const infinity: t = fXY::infinity as t;
-const neg_infinity: t = fXY::infinity as t;
+const NaN: float = 0.0/0.0;
+const infinity: float = 1.0/0.0;
+const neg_infinity: float = -1.0/0.0;
 
 mod consts {
-    const min_value: t = fXY::consts::min_value as t;
-    const max_value: t = fXY::consts::max_value as t;
-    const epsilon: t = fXY::consts::epsilon as t;
+    const zero: float = 0f;
+    const one: float = 1f;
+    const two: float = 2f;
+    const three: float = 3f;
+    const four: float = 4f;
+    const ten: float = 10f;
 
-    const pi: t = fXY::consts::pi as t;
-    const frac_pi_2: t = fXY::consts::frac_pi_2 as t;
-    const frac_pi_4: t = fXY::consts::frac_pi_4 as t;
-    const frac_1_pi: t = fXY::consts::frac_1_pi as t;
-    const frac_2_pi: t = fXY::consts::frac_2_pi as t;
-    const frac_2_sqrtpi: t = fXY::consts::frac_2_sqrtpi as t;
-    const sqrt2: t = fXY::consts::sqrt2 as t;
-    const frac_1_sqrt2: t = fXY::consts::frac_1_sqrt2 as t;
-    const e: t = fXY::consts::e as t;
-    const log2_e: t = fXY::consts::log2_e as t;
-    const log10_e: t = fXY::consts::log10_e as t;
-    const ln_2: t = fXY::consts::ln_2 as t;
-    const ln_10: t = fXY::consts::ln_10 as t;
+    const min_value: float = fXY::consts::min_value as float;
+    const max_value: float = fXY::consts::max_value as float;
+    const epsilon: float = fXY::consts::epsilon as float;
+
+    const pi: float = fXY::consts::pi as float;
+    const frac_pi_2: float = fXY::consts::frac_pi_2 as float;
+    const frac_pi_4: float = fXY::consts::frac_pi_4 as float;
+    const frac_1_pi: float = fXY::consts::frac_1_pi as float;
+    const frac_2_pi: float = fXY::consts::frac_2_pi as float;
+    const frac_2_sqrtpi: float = fXY::consts::frac_2_sqrtpi as float;
+    const sqrt2: float = fXY::consts::sqrt2 as float;
+    const frac_1_sqrt2: float = fXY::consts::frac_1_sqrt2 as float;
+    const e: float = fXY::consts::e as float;
+    const log2_e: float = fXY::consts::log2_e as float;
+    const log10_e: float = fXY::consts::log10_e as float;
+    const ln_2: float = fXY::consts::ln_2 as float;
+    const ln_10: float = fXY::consts::ln_10 as float;
 }
 
