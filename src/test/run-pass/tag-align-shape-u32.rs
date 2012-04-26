7d25b0ae077bc513b6b95dc675e148432db15787
Brian Anderson <banderson@mozilla.com>
2012-04-25 22:21:49 -0700

    testing

---
+++ src/test/run-pass/tag-align-shape-u32.rs
@@ -0,0 +1,18 @@
+//
+// See issue #1535
+
+enum a_tag {
+    a_tag(u64)
+}
+
+type t_rec = {
+    c8: u8,
+    t: a_tag
+};
+
+fn main() {
+    let x = {c8: 22u8, t: a_tag(44u64)};
+    let y = #fmt["%?", x];
+    #debug["y = %s", y];
+    assert y == "(22, a_tag(44))";
+}
